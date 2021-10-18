96a4680ea4ff73939a52d41b504e8f832fcd3c84
sunlong <736984575@qq.com>
2021-10-18 22:02:21 +0800

    孙龙111

---
+++ springboot_jdk1.8/src/main/java/LambdaTest/entity/Person.java
@@ -0,0 +1,11 @@
+package LambdaTest.entity;
+
+public class Person {
+    String firstName;
+    String lastName;
+    Person() {}
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ springboot_jdk1.8/src/main/java/LambdaTest/entity/Utils.java
@@ -0,0 +1,47 @@
+package LambdaTest.entity;
+
+public class Utils {
+    public void util1(){
+        System.out.println("普通方法1");
+    }
+    public static void util2(){
+        System.out.println("静态方法1");
+    }
+
+    private String id;
+    private String name;
+
+    public Utils() {
+        System.out.println("调用空参构造方法");
+    }
+
+    public Utils(String id, String name) {
+        this.id = id;
+        this.name = name;
+        System.out.println("调用有参构造方法");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Utils{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
