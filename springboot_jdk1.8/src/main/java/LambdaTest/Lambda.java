5fa2d8edf923d8893bbee19d8be636a27f8ecd0b
sunlong <736984575@qq.com>
2021-10-19 18:13:32 +0800

    集合遍历

--- springboot_jdk1.8/src/main/java/LambdaTest/Lambda.java
+++ springboot_jdk1.8/src/main/java/LambdaTest/Lambda.java
@@ -1,11 +1,15 @@
 package LambdaTest;
 
 import LambdaTest.entity.Utils;
+import com.sun.xml.internal.ws.util.StringUtils;
 import org.junit.Test;
 
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.IntConsumer;
+import java.awt.*;
+import java.nio.file.DirectoryStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.*;
 
 
 public class Lambda {
@@ -50,5 +54,55 @@ public class Lambda {
         tConsumer.accept(1);
     }
 
+    @Test
+    public  void test6(){
+        /**
+         * Predicate接口
+         *
+         * Predicate 接口只有一个参数，返回boolean类型。该接口包含多种默认方法来将Predicate组合成其他复杂的逻辑（比如：与，或，非）
+         */
+        Predicate<String> predicate = (s) -> s.length() > 0;
+        boolean b = predicate.test("");
+        System.out.println(b);
+        boolean xx = predicate.negate().test("xx");
+        System.out.println(xx);
 
+        Predicate<Boolean> nonNull = Objects::nonNull;
+        Predicate<String> isEmpty = String::isEmpty;
+        boolean b1 = isEmpty.test("");
+        System.out.println(b1);
+    }
+
+    @Test
+    public  void test7(){
+        /**
+         * Function 接口
+         *
+         * Function 接口有一个参数并且返回一个结果，并附带了一些可以和其他函数组合的默认方法（compose, andThen）：
+         */
+        Function<String, String> charAt = Utils::util2;
+        String s = charAt.apply("sssss");
+        System.out.println(s);
+
+    }
+
+    @Test
+    public  void test8(){
+        Function<String[], List<String>> list = Arrays::asList;
+        String[] strings = {"1","2","3"};
+        List<String> nums = list.apply(strings);
+        //  第一种遍历方式：
+
+        nums.forEach(s->{
+            System.out.println(s);
+        });
+        System.out.println("=====================");
+
+        //第二种遍历方式：
+        nums.forEach(s-> System.out.println(s));
+
+        System.out.println("==========================");
+        //第三种遍历方式：
+        nums.forEach(System.out::println);
+    }
 }
---
+++ springboot_jdk1.8/src/main/java/LambdaTest/LambdaCollections.java
@@ -0,0 +1,55 @@
+package LambdaTest;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaCollections {
+    /**
+     * stream
+     * 表示能应用在一组元素上一次执行的操作序列。
+     * Stream 操作分为中间操作或者最终操作两种，最终操作返回一特定类型的计算结果，而中间操作返回Stream本身，这样你就可以将多个操作依次串起来。
+     * Stream 的创建需要指定一个数据源，比如 java.util.Collection的子类，List或者Set， Map不支持。Stream的操作可以串行执行或者并行执行。
+     * Java 8扩展了集合类，可以通过 Collection.stream() 或者 Collection.parallelStream() 来创建一个Stream。
+     */
+    private static List list = new ArrayList();
+    private static List<Integer> list1 = new ArrayList<>();
+    static{
+        list.add(1);
+        list.add(2);
+        list.add("3");
+        list.add(true);
+        list.add('1');
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+    }
+
+    @Test
+    public void test1(){
+        //简单便利
+        list.forEach(System.out::println);
+        System.out.println("-----------------------");
+        List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.stream().forEach(s->{s=s+1;
+            System.out.println(s);
+        });
+        //由于是以流方式，所以怎么操作也不改变原来的集合
+        System.out.println("-----------------------");
+        list3.forEach(System.out::println);
+    }
+
+    @Test
+    public void test2(){
+        //filter 过滤
+        list.stream().filter(s -> s instanceof Integer).forEach(System.out::println);
+        //System.out.println("----------------------------------");
+       // list1.stream().filter(s ->{ return s instanceof Integer;}).forEach(System.out::println);
+    }
+}
