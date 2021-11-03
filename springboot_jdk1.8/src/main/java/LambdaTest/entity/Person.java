5e20242bdc42a7e4a8b02b2526f4fc7069896c3c
sunlong <736984575@qq.com>
2021-11-03 11:21:59 +0800

    测试自定义starter-自定义装配

--- springboot_jdk1.8/src/main/java/LambdaTest/entity/Person.java
+++ springboot_jdk1.8/src/main/java/LambdaTest/entity/Person.java
@@ -3,9 +3,25 @@ package LambdaTest.entity;
 public class Person {
     String firstName;
     String lastName;
-    Person() {}
-    Person(String firstName, String lastName) {
+    public Person() {}
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
--- springboot_jdk1.8/src/main/java/test.java
+++ springboot_jdk1.8/src/main/java/test.java
@@ -1,8 +1,38 @@
+import LambdaTest.entity.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import spring.boot.spring.boot.JsonSerial;
+import spring.boot.spring.boot.MyAutoConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MyAutoConfiguration.class})
 public class test {
+    @Autowired
+    private JsonSerial jsonSerial;
+
+    @Autowired
+    private ApplicationContext aa;
+
     public static void main(String[] args) {
         JsonSerial a = new JsonSerial();
-        //a.serial()
+        String s = a.serial(new Person("1", "22"));
+        System.out.println(s);
+    }
+
+    @Test
+    public void test1(){
+        String s = jsonSerial.serial(new Person("1", "22"));
+        System.out.println(s);
+    }
+    @Test
+    public void test2(){
+        JsonSerial jsonSerial = (JsonSerial) aa.getBean("jsonSerial");
+        String s = jsonSerial.serial(new Person("1", "22"));
+        System.out.println(s);
     }
 }
